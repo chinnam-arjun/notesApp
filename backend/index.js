337ccd42c29132b1d617c8f93f6d5660432eb17d
chinnam-arjun <chinnammallikarjunarao.99@gmail.com>
2025-10-21 22:33:14 +0530

    Change CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://notes-app-frontend-e9xv.onrender.com",
     credentials: true
 }));
 app.use(bodyparser.json());
