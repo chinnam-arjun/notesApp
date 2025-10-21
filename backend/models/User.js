1b6b9b1faac0349a974e532ec1d5d539054cf537
chinnam-arjun <chinnammallikarjunarao.99@gmail.com>
2025-10-21 21:50:20 +0530

    removed unused comments-[practised code]

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,34 +1,3 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema({
-//     username:{
-//         type: String,
-//         required: true,
-//     },
-//     email:{
-//         type: String,
-//         required: true,
-//         unique: true,
-//     },
-//     password:{
-//         type: String,
-//         required: true,
-//     },
-//     createdAt:{
-//         type: Date,
-//         default: Date.now,
-//     },
-//     isAdmin:{
-//         type: Boolean,
-//         default: false,
-//     },
-//     isRestricted:{
-//         type: Boolean,
-//         default: false,
-//     }
-// }, {timestamps: true});
-
-// export const User = mongoose.model("User", userSchema);
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
